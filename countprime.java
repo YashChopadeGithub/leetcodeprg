1bd9ec9255ac3c3863203bb051a2dd11947eff85
Yash Chopade <chopadeyash05@gmail.com>
2024-09-01 22:10:04 +0530

    Add files via upload

---
+++ countprime.java
@@ -0,0 +1,29 @@
+public class countprime {
+    public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int p = 2; p * p < n; p++) {
+            if (isPrime[p]) {
+                for (int i = p * p; i < n; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
